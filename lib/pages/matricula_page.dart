8e52e0f78f1c78cff64b3ff52cf4fd277b0054b0
Ahrend <caguilar@agrokasa.com.pe>
2024-05-26 11:19:05 -0500

    initState

--- lib/pages/matricula_page.dart
+++ lib/pages/matricula_page.dart
@@ -4,7 +4,12 @@ import 'package:flutter_matriculasapp/models/matricula_model.dart';
 import 'package:flutter_matriculasapp/pages/alumno_card.dart';
 import 'package:flutter_matriculasapp/models/alumno_model.dart';
 
-class MatriculaPage extends StatelessWidget{
+class MatriculaPage extends StatefulWidget{
+  @override
+  State<MatriculaPage> createState() => _MatriculaPageState();
+}
+
+class _MatriculaPageState extends State<MatriculaPage> {
   Matricula nuevomatricola = Matricula(alumno: Alumno("Jeremy", "jeremy@gmail.com", "45858565"),
   fecha: "26/05/2024",carrera: Carrera(titulo: "Ing. Sistemas",duracion: "5años"));
 
@@ -15,6 +20,7 @@ class MatriculaPage extends StatelessWidget{
   ];
 
   List<Widget> tilesList = [];
+
   generateListTile(){
     alumnoList.forEach((element){
       tilesList.add(AlumnoCard(name: element.nombre, institucion: "tecsup"));
@@ -22,13 +28,28 @@ class MatriculaPage extends StatelessWidget{
   }
 
   @override
-  Widget build(BuildContext context){
+  void initState() {
     generateListTile();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
+      floatingActionButton: FloatingActionButton
+      (onPressed: (){
+        Alumno auxAlumno = Alumno("Mario", "mario@gmail.com", "85458565");
+        alumnoList.add(auxAlumno);
+        generateListTile();
+        setState(() { });
+      },
+      child: Icon(Icons.add),),
       appBar: AppBar(title: Text("Matricula"),
       ),
       body: Column(children: [Text("Mis Alumnos"),
       ...tilesList,
+      ...alumnoList.map((mandarina)=> AlumnoCard(name: mandarina.nombre, institucion: "PUCP")
+      ).toList(),
       ],
       ),
     );
