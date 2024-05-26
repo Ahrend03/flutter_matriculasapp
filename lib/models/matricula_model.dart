aff041e6a31be7fbb8dd765085a287239a420319
Ahrend <caguilar@agrokasa.com.pe>
2024-05-26 11:01:47 -0500

    generateListTile

--- lib/models/matricula_model.dart
+++ lib/models/matricula_model.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_matriculasapp/models/alumno_model.dart';
+import 'package:flutter_matriculasapp/models/carrera_model.dart';
 
 class Matricula{
   Alumno alumno;
   String? fecha;
   String hora;
-  Matricula({required this.alumno,this.fecha,this.hora = "10:00"});
+  Carrera carrera;
+  Matricula({
+    required this.alumno,
+    this.fecha,
+    this.hora = "10:00",
+    required this.carrera
+    });
 }
--- lib/pages/matricula_page.dart
+++ lib/pages/matricula_page.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_matriculasapp/models/carrera_model.dart';
 import 'package:flutter_matriculasapp/models/matricula_model.dart';
 import 'package:flutter_matriculasapp/pages/alumno_card.dart';
 import 'package:flutter_matriculasapp/models/alumno_model.dart';
 
 class MatriculaPage extends StatelessWidget{
-  Matricula nuevomatricola = new Matricula(alumno: Alumno("Jeremy", "jeremy@gmail.com", "45858565"),
-  fecha: "26/05/2024");
+  Matricula nuevomatricola = Matricula(alumno: Alumno("Jeremy", "jeremy@gmail.com", "45858565"),
+  fecha: "26/05/2024",carrera: Carrera(titulo: "Ing. Sistemas",duracion: "5años"));
+
+  List<Alumno> alumnoList = [
+    Alumno("Juanito", "juanito@gmail.com", "123456789"),
+    Alumno("Carlos", "carlos@gmail.com", "85859858"),
+    Alumno("Ana", "ana@gmail.com", "85859595")
+  ];
+
+  List<Widget> tilesList = [];
+  generateListTile(){
+    alumnoList.forEach((element){
+      tilesList.add(AlumnoCard(name: element.nombre, institucion: "tecsup"));
+    });
+  }
+
   @override
   Widget build(BuildContext context){
+    generateListTile();
     return Scaffold(
       appBar: AppBar(title: Text("Matricula"),
       ),
-      body: Column(children: [
-        AlumnoCard(name:"Antonio Aguilar", institucion:"Sociedad Agricola Drokasa"),
-        AlumnoCard(name: "Sheyla Neyra",institucion: "Cerro Lindo"),
-      ],),
+      body: Column(children: [Text("Mis Alumnos"),
+      ...tilesList,
+      ],
+      ),
     );
   }
 }
